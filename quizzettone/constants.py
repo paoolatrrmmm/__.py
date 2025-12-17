ed95ca0c2c3bae31afe212a405a865df212c06cf
paoolatrrmmm <paoolatrm@gmail.com>
2025-12-17 19:54:48 +0100

    up!

---
+++ quizzettone/constants.py
@@ -0,0 +1,3 @@
+BASE_URL: str = "https://raw.githubusercontent.com/paoolatrrmmm/__.py/main/quizzettone"
+LISTA_DOMANDE_URL: str = f"{BASE_URL}/domande.txt"
+BASE_DOMANDA_SINGOLA_URL: str = f"{BASE_URL}/domande_risposte"
--- quizzettone/main.py
+++ quizzettone/main.py
@@ -1,136 +1,94 @@
-def mostra_feedback(messaggio: str) -> None:
-    """
-    Restituisce il feedback formattato nella maniera desiderata.
-    """
-    simbol: str = "*"*30
-    print(f"""
-{simbol}
-{messaggio}
-{simbol}
-""")
+from data.services import (
+    get_lista_domande_e_risposte, 
+    valida_scelta, 
+    is_risposta_esatta, 
+    get_numero_domanda_corrente, 
+    get_counter_aggiornato,
+    genera_statistiche,
+    calcola_percentuale,
+    verifica_superamento,
+    recupera_dati_domanda,
+    aggiorna_lista_risultati
+)
+from ui.console import (
+    mostra_feedback, 
+    mostra_domanda, 
+    print_numero_domanda, 
+    print_gioco_terminato, 
+    genera_feedback, 
+    raccogli_risposta,
+    gestisci_menu_fine_gioco,
+    mostra_risultati_finali
+)
 
-def is_risposta_esatta(scelta: str, risposta_esatta: str) -> bool:
-    if scelta.upper() == risposta_esatta:
-        return True
-    else:
-        return False
+from constants import LISTA_DOMANDE_URL, BASE_DOMANDA_SINGOLA_URL
 
-
-def genera_feedback(is_corretta: bool) -> str:
-    """
-    Restituisce il messaggio che indica all'utente se ha indovinato la risposta oppure no.
-    Questa funzione viene eseguita solo se la funzione di validazione restituisce true. 
-    """
-    if is_corretta == True:
-        return "Hai indovinato!"
-    else:
-        return "Non hai indovinato."
-
-def valida_scelta(scelta: str) -> bool:
-    """
-    Questa funzione prende un valore di tipo stringa e verifica che la risposta sia una delle opzioni tra A, B, C e D. 
-    Se la risposta è una stringa vuota, restituisce false, idem se la risposta non è una di quelle sopra elencate.
-    """
-    scelta_tmp = scelta.upper()
-    if scelta_tmp == "A" or scelta_tmp == "B" or scelta_tmp == "C" or scelta_tmp == "D":
-        return True
-    else: 
-        return False
-
-def mostra_domanda(domanda: str) -> None: 
-    """
-    Questa funzione restituisce la domanda e le opzioni della riposta. 
-    """
-    
-    print(domanda)
-
-def raccogli_risposta() -> str:
-    """
-    Questa funzione si occupa solamente di prendere l'input dell'utente. 
-    Il controllo di tale valore avverrà attraverso una funzione dedicata.
-    """ 
-    return input("Inserisci la tua scelta: ")
-    
-
-def leggi_file(file_path: str) -> str:
-    with open(file_path, "r") as file:
-        content = file.read()
-        return content
-
-def estrai_index(content: str) -> int: 
-    return content.index("£")
-
-def estrai_domanda(content: str, index: int) -> str:
-    return content[0:index]
-
-def estrai_risposta(content: str, index: int) -> str:
-    return content[index+1:]
-
-def estrai_lista_domande(file_path: str) -> list[str]:
-    lista_domande: list[str] = []
-    with open(file_path, "r") as f:
-        for i in f:
-            lista_domande.append(i.strip())
-    return lista_domande 
-
-def genera_statistiche(risultato_finale: list[dict[str, str | bool]]) -> dict[str, int]:
-    statistica: dict[str, int] = {}
-
-    risposte_esatte: int = 0
-    risposte_non_esatte: int = 0
-
-    for i in risultato_finale: 
-        if i["risposta_corretta"]:
-            risposte_esatte += 1
-        else:
-            risposte_non_esatte += 1
-
-    statistica["risposte_esatte"] = risposte_esatte
-    statistica["risposte_non_esatte"] = risposte_non_esatte
-    return statistica
 
 def main():
-    lista_domande: list[str] = []
+    lista_domande = get_lista_domande_e_risposte(LISTA_DOMANDE_URL)
     risultato_finale: list[dict[str, str | bool]] = []
-    domanda_e_risposta: dict[str, str] = {"domanda" : None, "risposta" : None}
-    lista_domande = estrai_lista_domande("domande.txt")
 
-    counter_domanda_corrente: int = 0
+    counter: int = 0
+    totale_domande: int = len(lista_domande)
 
-    lista_domande_length: int = len(lista_domande)
+    while counter <= totale_domande:
 
-    while counter_domanda_corrente < lista_domande_length:
-        content: str = leggi_file(f"domande&risp/{lista_domande[counter_domanda_corrente]}")
-        index: int = estrai_index(content)
-        domanda_e_risposta["domanda"] = estrai_domanda(content, index)
-        domanda_e_risposta["risposta"] = estrai_risposta(content, index)
+        if counter == totale_domande:
+            nuovo_indice = gestisci_menu_fine_gioco(counter, totale_domande, risultato_finale)
+            if nuovo_indice is None: 
+                break
+            counter = nuovo_indice
+            continue
+        
+        # recupero dei dati
+        dati_correnti = recupera_dati_domanda(f"{BASE_DOMANDA_SINGOLA_URL}/{lista_domande[counter]}") 
 
-        mostra_domanda(domanda_e_risposta["domanda"])
+        # presentazione domande
+        domanda_corrente: int = get_numero_domanda_corrente(counter)
+        print_numero_domanda(domanda_corrente, totale_domande)
+        mostra_domanda(dati_correnti["domanda"])
 
+        # input dell'utente
         risposta_utente: str = raccogli_risposta()
-
-        is_risposta_valid: bool = valida_scelta(risposta_utente)
 
         feedback: str = ""
 
-        if is_risposta_valid:
-            risultato: dict[str, str | bool] = {}
-            is_risposta_corretta: bool = is_risposta_esatta(risposta_utente, domanda_e_risposta["risposta"])
-            feedback = genera_feedback(is_risposta_corretta)
-            risultato["domanda"] = lista_domande[counter_domanda_corrente]
-            risultato["risposta_corretta"] = is_risposta_corretta
-            risultato_finale.append(risultato)
-            counter_domanda_corrente += 1
-        else: 
-            feedback = "Inserisci solo la risposta tra le opzioni elencate"
+        if valida_scelta(risposta_utente):
+            esatta: bool = is_risposta_esatta(risposta_utente, dati_correnti["risposta"])
+            mostra_feedback(genera_feedback(esatta))
 
-        mostra_feedback(feedback)
+            risultato_corrente = {
+                "domanda" : lista_domande[counter],
+                "risposta_corretta" : esatta,
+                "scelta_utente" : risposta_utente.upper()
+            }
+
+            aggiorna_lista_risultati(risultato_finale, risultato_corrente, counter)
+
+
+        else: 
+            mostra_feedback("Inserisci solo la risposta tra le opzioni elencate")
+
+        # navigazione domanda (successiva / precedente)
+        if counter > 0:
+            input_prev_next: str = input("Digita 'P' per andare alla domanda precedente oppure qualsiasi altro tasto per continuare: ")
+            counter = get_counter_aggiornato(counter, input_prev_next)
+        else:
+            counter += 1 
+
+    # statistiche finali
+    print_gioco_terminato()
 
     statistiche: dict[str, int] = genera_statistiche(risultato_finale)
+    esatte = statistiche["risposte_esatte"]
+    errate = statistiche["risposte_non_esatte"]
+    totale_domande_fatte = esatte + errate
+    perc = calcola_percentuale(esatte, totale_domande_fatte)
+    is_superato = verifica_superamento(perc)
+    
+    # Questa riga mostra il punteggio finale formattato
+    mostra_risultati_finali(esatte, errate,
+        totale_domande_fatte, perc, is_superato)
 
-    print(statistiche["risposte_esatte"])
-    print(statistiche["risposte_non_esatte"])   
-
-
-# Entry point del nostro programma
-main()
+if __name__ == "__main__": 
+    main()
